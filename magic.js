0b701f7826ddfe732eebafc50322d08cf8f62137
austin-area61 <austinonyango.area61@gmail.com>
2024-06-26 11:03:49 +0300

    the prediction is made :-0!

--- magic.js
+++ magic.js
@@ -1,5 +1,37 @@
-let userName = '';
-let userQuestion = 'How to figure out life';
+let userName = ""; // User can enter their name here if they wish
+let userQuestion = "Will I get a promotion this year?"; // Example question, replace with the user's question
+
 console.log(userName ? `${userName} asked: ${userQuestion}` : `The user asked: ${userQuestion}`);
+
 let randomNumber = Math.floor(Math.random() * 8);
-console.log(`Random number generated: ${randomNumber}`);
+
+let eightBall = '';
+
+switch(randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+    break;
+}
+
+console.log(`The Magic Eight Ball says: ${eightBall}`);
