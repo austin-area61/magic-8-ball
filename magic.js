0f92d83c66fdc316988c15ddc6381d650a700598
austin-area61 <austinonyango.area61@gmail.com>
2024-06-26 10:42:05 +0300

    initial commit

---
+++ magic.js
@@ -0,0 +1,4 @@
+let userName = '';
+let userQuestion = 'How to figure out life';
+console.log(userName ? `${userName} asked: ${userQuestion}` : `The user asked: ${userQuestion}`);
+let randomNumber = Math.floor(Math.random() * 8);
