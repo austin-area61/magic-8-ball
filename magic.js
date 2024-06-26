bd67cb8dc7fa17aa3600c1237e2abf9beaa064fb
austin-area61 <austinonyango.area61@gmail.com>
2024-06-26 10:45:12 +0300

    create a ternary expression that decides what to do if the user enters a name or not. If the user enters a name — like 'Jane' — use string interpolation to log Hello, Jane! to the console. Otherwise, simply log Hello!.

--- magic.js
+++ magic.js
@@ -2,3 +2,4 @@ let userName = '';
 let userQuestion = 'How to figure out life';
 console.log(userName ? `${userName} asked: ${userQuestion}` : `The user asked: ${userQuestion}`);
 let randomNumber = Math.floor(Math.random() * 8);
+console.log(`Random number generated: ${randomNumber}`);
